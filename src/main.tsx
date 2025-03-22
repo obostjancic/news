4540cd40264d8739765fe2bcfdacb8c5d09f8452
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-03-22 20:24:15 +0100

    add sentry tracing

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import * as Sentry from "@sentry/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracePropagationTargets: [
+    "localhost",
+    /^https:\/\/news\.ognjenbostjancic\.com/,
+  ],
+  tracesSampleRate: 1.0,
 });
 
 createRoot(document.getElementById("root")!).render(
